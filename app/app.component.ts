2b2634dc42468f35c30309a79ebc25eaf8cbaca1
prabhu-raja <prabhuraja@gmail.com>
2018-05-22 11:20:01 -0400

    Init Kicked off 🎬

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
-    </div>
+  <div>
+    {{title}}
+  </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = 'Ulti Ang';
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   styleUrls: ['app.component.scss'],
+//   template: `
+//     <div class="app">
+//       Hello!
+//     </div>
+//   `
+// })
+// export class AppComponent {
+
+// }
