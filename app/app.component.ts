d4533495d1c317343870dd7dd7453bad8bee47ff
prabhu-raja <prabhuraja@gmail.com>
2018-05-22 15:49:50 -0400

    🧢Property Binding added

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,22 @@ import { Component } from "@angular/core";
   styleUrls: ['app.component.scss'],
   template: `
   <div>
-    {{title}}
+    <h1 [innerHTML]="title"></h1>
+    <img [src]="logo">
+    <input type="text" [value]="name">
+    <div>{{name}}</div> 
   </div>
   `
 })
 
 export class AppComponent {
   title: string;
+  logo: string;
+  name: string;
+
   constructor() {
     this.title = 'Ulti Ang';
+    this.logo = 'img/logo.svg';
+    this.name = 'PradeeP';
   }
 }
