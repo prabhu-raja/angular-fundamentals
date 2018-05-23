4e18ccb4609a63ba2bbb04e0ec5717897dc9879b
prabhu-raja <prabhuraja@gmail.com>
2018-05-23 10:37:25 -0400

    ngFor with structural and template ⚓️

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,33 +1,56 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
   <div class="app">
-    <input 
-      type="text" 
-      [value]="name"
-      (input)="handleChange($event.target.value)">
-    
-      <template [ngIf]="name.length > 2">
-        <div>
-          Search for.. {{name}}
-        </div>
+    <h3>Airline Passengers</h3>
+    <ul>
+      <template ngFor let-psgr [ngForOf]="passengers" let-i="index" >
+        <li>
+          {{ i }} - {{ psgr.fullname }}
+        </li>
       </template>
-    
-      <div *ngIf="name.length > 2">
-        Searching for.. {{name}}
-      </div>
+    </ul>
+
+    <h3>Airline Passengers</h3>
+    <ul>
+      <li *ngFor="let psgr of passengers; let i = index">
+        {{ i }} - {{ psgr.fullname }}
+      </li>
+    </ul>
+
   </div>
   `
 })
 
 export class AppComponent {
   
-  name: string = '';
-
-  handleChange(val: string) {
-    this.name = val;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
