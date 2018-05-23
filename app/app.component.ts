9c9b01eff9b64e516c3dd4292f3f334f53057ea2
prabhu-raja <prabhuraja@gmail.com>
2018-05-23 14:49:53 -0400

    Added pipes 🌵

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 
 interface Passenger {
-  id: number,
-  fullname: string,
-  checkedIn: boolean
+  id         : number,
+  fullname   : string,
+  checkedIn  : boolean,
+  checkInDate: number | null;
 }
 @Component({
   selector: 'app-root',
@@ -17,77 +18,45 @@ interface Passenger {
         class="status"
         [class.checked-in]="psgr.checkedIn">
       </span>
-        {{ i }} - {{ psgr.fullname }}
+      {{ i }} - {{ psgr.fullname }}
+      <p>{{psgr | json}}</p>
+      <div class="date">
+        Check in date: {{psgr.checkInDate ? (psgr.checkInDate | date:'yMMMMd') : 'Not Checked in'}}
+      </div>
       </li>
     </ul>
-    <!-- -->
-    <hr>
-    <h3>Airline Passengers - [ngClass]</h3>    
-    <ul>
-      <li *ngFor="let psgr of passengers; let i = index">
-      <span 
-        class="status"
-        [ngClass]="{
-          'checked-in': psgr.checkedIn,
-          'checked-out': !psgr.checkedIn
-        }">
-      </span>
-        {{ i }} - {{ psgr.fullname }}
-      </li>
-    </ul>
-    <!-- -->
-    <hr>
-    <h3>Airline Passengers - [style]</h3>    
-    <ul>
-      <li *ngFor="let psgr of passengers; let i = index">
-      <span 
-        class="status"
-        [style.backgroundColor]="(psgr.checkedIn ? '#2ecc71': '#c0392b')">
-      </span>
-        {{ i }} - {{ psgr.fullname }}
-      </li>
-    </ul>
-    <!-- -->
-    <hr>
-    <h3>Airline Passengers - [ngStyle]</h3>    
-    <ul>
-      <li *ngFor="let psgr of passengers; let i = index">
-      <span 
-        class="status"
-        [ngStyle]="{
-          backgroundColor: (psgr.checkedIn ? '#2ecc71': '#c0392b')
-        }">
-      </span>
-        {{ i }} - {{ psgr.fullname }}
-      </li>
-    </ul>
-
-
   </div>
   `
 })
 
 export class AppComponent {
   
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true
+  passengers: Passenger[] = [
+  {
+    id         : 1,
+    fullname   : 'Stephen',
+    checkedIn  : true,
+    checkInDate: 1490742000000
   }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false
+    id         : 2,
+    fullname   : 'Rose',
+    checkedIn  : false,
+    checkInDate: null
   }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true
+    id         : 3,
+    fullname   : 'James',
+    checkedIn  : true,
+    checkInDate: 1491606000000
   }, {
-    id: 4,
-    fullname: 'Louise',
-    checkedIn: true
+    id         : 4,
+    fullname   : 'Louise',
+    checkedIn  : true,
+    checkInDate: 1488412800000
   }, {
-    id: 5,
-    fullname: 'Tina',
-    checkedIn: false
-  }];
+    id         : 5,
+    fullname   : 'Tina',
+    checkedIn  : false,
+    checkInDate: null
+  }
+  ];
 }
