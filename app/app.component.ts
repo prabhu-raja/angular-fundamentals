fb06d3e52e9941c37311122bfde976b71288faa3
prabhu-raja <prabhuraja@gmail.com>
2018-05-23 15:04:55 -0400

    Safe Navigation 👨‍✈️

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,15 @@ interface Passenger {
   id         : number,
   fullname   : string,
   checkedIn  : boolean,
-  checkInDate: number | null;
+  checkInDate: number | null,
+  children   : Child[] | null
 }
+
+interface Child {
+  name: string,
+  age : number
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
@@ -14,15 +21,23 @@ interface Passenger {
     <h3>Airline Passengers - [class]</h3>
     <ul>
       <li *ngFor="let psgr of passengers; let i = index">
-      <span 
-        class="status"
-        [class.checked-in]="psgr.checkedIn">
-      </span>
-      {{ i }} - {{ psgr.fullname }}
-      <p>{{psgr | json}}</p>
-      <div class="date">
-        Check in date: {{psgr.checkInDate ? (psgr.checkInDate | date:'yMMMMd') : 'Not Checked in'}}
-      </div>
+        <span 
+          class="status"
+          [class.checked-in]="psgr.checkedIn">
+        </span>
+
+        {{ i }} - {{ psgr.fullname }}
+
+        <!--<p>{{psgr | json}}</p>-->
+
+        <div class="date">
+          Check in date: {{psgr.checkInDate ? (psgr.checkInDate | date:'yMMMMd') : 'Not Checked in'}}
+        </div>
+
+        <div class="children">
+          Children: {{psgr.children?.length || 0}}
+        </div>
+
       </li>
     </ul>
   </div>
@@ -32,31 +47,42 @@ interface Passenger {
 export class AppComponent {
   
   passengers: Passenger[] = [
-  {
-    id         : 1,
-    fullname   : 'Stephen',
-    checkedIn  : true,
-    checkInDate: 1490742000000
-  }, {
-    id         : 2,
-    fullname   : 'Rose',
-    checkedIn  : false,
-    checkInDate: null
-  }, {
-    id         : 3,
-    fullname   : 'James',
-    checkedIn  : true,
-    checkInDate: 1491606000000
-  }, {
-    id         : 4,
-    fullname   : 'Louise',
-    checkedIn  : true,
-    checkInDate: 1488412800000
-  }, {
-    id         : 5,
-    fullname   : 'Tina',
-    checkedIn  : false,
-    checkInDate: null
-  }
+    {
+      id         : 1,
+      fullname   : 'Stephen',
+      checkedIn  : true,
+      checkInDate: 1490742000000,
+      children   : null
+    }, 
+    {
+      id         : 2,
+      fullname   : 'Rose',
+      checkedIn  : false,
+      checkInDate: null,
+      children   : [
+        { name: 'Ted', age: 12 },
+        { name: 'Chloe', age: 7 }
+      ]
+    }, {
+      id         : 3,
+      fullname   : 'James',
+      checkedIn  : true,
+      checkInDate: 1491606000000,
+      children   : null
+    }, 
+    {
+      id         : 4,
+      fullname   : 'Louise',
+      checkedIn  : true,
+      checkInDate: 1488412800000,
+      children   : [{ name: 'Jessica', age: 1 }]
+    }, 
+    {
+      id         : 5,
+      fullname   : 'Tina',
+      checkedIn  : false,
+      checkInDate: null,
+      children   : null
+    }
   ];
 }
