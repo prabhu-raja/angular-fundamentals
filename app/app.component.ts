6e96f8421527dcd2cf715633e89cdc6b365e9f01
prabhu-raja <prabhuraja@gmail.com>
2018-05-25 18:17:14 -0400

    Added Data Services 🧞‍♂️ and Dependency Injection 👩🏻‍⚕️

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from "@angular/core";
 
-
-
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
   <div class="app">
-   <passenger-dashboard></passenger-dashboard>
+    <passenger-dashboard></passenger-dashboard>
   </div>
   `
 })
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";  
+import { PassengerDashboardService } from './../../passenger-dashboard.service';
 
 @Component({
     selector: 'passenger-dashboard',
@@ -28,47 +29,10 @@ export class PassengerDashboardComponent implements OnInit {
 
     passengers: Passenger[];
 
+    constructor(private passengerService: PassengerDashboardService) {}
+
     ngOnInit() {
-        this.passengers = [
-            {
-              id         : 1,
-              fullname   : 'Stephen',
-              checkedIn  : true,
-              checkInDate: 1490742000000,
-              children   : null
-            }, 
-            {
-              id         : 2,
-              fullname   : 'Rose',
-              checkedIn  : false,
-              checkInDate: null,
-              children   : [
-                { name: 'Ted', age: 12 },
-                { name: 'Chloe', age: 7 }
-              ]
-            }, 
-            {
-              id         : 3,
-              fullname   : 'James',
-              checkedIn  : true,
-              checkInDate: 1491606000000,
-              children   : null
-            }, 
-            {
-              id         : 4,
-              fullname   : 'Louise',
-              checkedIn  : true,
-              checkInDate: 1488412800000,
-              children   : [{ name: 'Jessica', age: 1 }]
-            }, 
-            {
-              id         : 5,
-              fullname   : 'Tina',
-              checkedIn  : false,
-              checkInDate: null,
-              children   : null
-            }
-        ];
+        this.passengers = this.passengerService.getPassengers();
     }
 
     handleEdit(evt: Passenger) {
@@ -85,7 +49,7 @@ export class PassengerDashboardComponent implements OnInit {
 
     handleRemove(evt: Passenger) {
         console.log('⚡️🙅‍♂️', evt);
-        this.passengers = this.passengers.filter((psgr:Passenger) => psgr.id != evt.id)
+        this.passengers = this.passengers.filter((psgr:Passenger) => psgr.id != evt.id);
     }
 
 }
